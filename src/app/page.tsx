c57a020c2ab7c39a98841dd4af5db5c2c8a79c85
geun99 <lich1115@naver.com>
2024-06-20 17:50:55 +0900

    [feat] babel 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import { MainStyle } from "./style";
+"use client";
+
+import styled from "styled-components";
+
 import SearchBox from "./components/common/searchBox";
 import Image from "next/image";
 import YETI from "../../public/yeti.png";
 
-const Main = () => {
+const Page = () => {
   return (
     <MainStyle>
       <h1>YETI.GG</h1>
@@ -13,4 +16,34 @@ const Main = () => {
   );
 };
 
-export default Main;
+export const MainStyle = styled.div`
+  display: flex;
+  padding: 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  h1 {
+    color: white;
+    font-size: 6rem;
+    padding-top: 20px;
+  }
+  img {
+    padding-top: 40px;
+    width: 300px;
+    height: auto;
+  }
+  @media (max-width: 768px) {
+    h1 {
+      font-size: 4rem;
+    }
+
+    img {
+      padding-top: 10px;
+      width: 250px;
+      height: auto;
+    }
+  }
+`;
+
+export default Page;
