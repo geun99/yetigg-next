d2b777d601874f71052f10b897bf646c7ddf5fa1
geun99 <lich1115@naver.com>
2024-06-20 20:39:45 +0900

    [fix] babel삭제, next config styled-component 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     domains: ["maplestory.io"],
   },
